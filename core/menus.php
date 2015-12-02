643076108cb28f7bfbc22fffc62e6e2918261787
zeusgrs <zeusgrs@yahoo.gr>
2015-12-02 15:45:17 +0200

    create main menus class and methods

---
+++ core/menus.php
@@ -0,0 +1,35 @@
+<?php
+class Menus {
+    /**
+     * create global main menu
+     */
+    public function Main() {
+        echo "<div class='mainMenu'>";
+        echo "<ul>";
+        $this->mainMenuItem("Games","http://webtouch.gr");
+        $this->mainMenuItem("Tools");
+        $this->mainMenuItem("Music");
+        $this->mainMenuItem("Photos");
+        $this->mainMenuItem("Internet");
+        echo "</ul>";
+        echo "</div>";
+    }
+    
+    /**
+     * $name convert to lowercase char for using good in icon , css
+     * @param type $name = name of menu item
+     * @param type $url = url link for menu item
+     */
+    function mainMenuItem($name,$url="#"){
+        echo "<a href='$url'>
+                <li id='".strtolower($name)."'>
+                    <div class='".strtolower($name)."'>
+                        <img src='images/icons/icon_".strtolower($name).".png' width='25px'>
+                    </div>
+                    <div class='mtitle'>
+                        $name
+                    </div>
+                </li>
+              </a>";
+    }
+}
--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 include_once 'core/pages.php';
 include_once 'core/databases.php';
+include_once 'core/menus.php';
 
 $HtmlPage = new HtmlPage();
 $Database = new Database();
+$Menus = new Menus();
 
 $HtmlPage->HeadStart("WebAppStore ver 0.1");
 
@@ -15,7 +17,7 @@ $HtmlPage->HeadEnd();
 
 $HtmlPage->BodyStart();
 
-echo "Hello World";
+$Menus->Main();
 
 $HtmlPage->BodyEnd();
 
