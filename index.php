7e19a82d052222ed97cf4251dfb8ff5dcd4e4fd9
zeusgrs <zeusgrs@yahoo.gr>
2015-12-03 21:44:28 +0200

    start design app page include qrcode generator from https://github.com/jeromeetienne/jquery-qrcode

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $HtmlPage->HeadStart("WebAppStore ver 0.1");
 $HtmlPage->Stylesheet("css/style.css");
 $HtmlPage->Jquery();
 $HtmlPage->Jquery("js/qrcode.js");
+
 $HtmlPage->Lightbox();
         
 $HtmlPage->HeadEnd();
@@ -71,13 +72,24 @@ if($appid == null){
         echo "<div id='appQRcode'></div>";        
         
     echo "</div>";
-
-    echo "<div class='appMedia'>";
     
+    echo "<div class='clear'></div>";
+    
+    echo "<div class='appMedia'>";
+    $title = "Webtouch Voip Client";
+    $img1 = "images/app/webtouch_voip_client_1.jpeg";
+    echo "<a href='$img1' data-lightbox='app-set' title='$title'><img src='$img1'></a>";
+    $img2 = "images/app/webtouch_voip_client_2.jpeg";
+    echo "<a href='$img2' data-lightbox='app-set' title='$title'><img src='$img2'></a>";
     echo "</div>";    
     
-    echo "<div class='appBody'>";
-    
+    echo "<div class='clear'></div>";
+
+    echo "<div class='appDescription'>";
+    echo "<h2>Description</h2></br>";
+    echo "Για να συνδεθείτε στο πρόγραμμα θα πρέπει πρώτα να δημιουργήσετε έναν δωρεάν λογαριασμό χρήστη εδώ http://webtouch.gr/?page_id=2102 και να αποκτήσετε δωρεάν κλήσεις περίπου 10 λεπτών.<br/>
+WEBTouch VOIP Client είναι μια voip εφαρμογή για Android , η οποία υποστηρίζει ένα ευρύ φάσμα υπηρεσιών VoIP. Απλά συνδεθείτε χρησιμοποιώντας τον υπάρχοντα λογαριασμό σας και αρχίστε να εξοικονομείτε χρήματα για εθνικές και διεθνείς κλήσεις , όπου και να είστε , όποτε θέλετε!<br/>
+Για να ξεκινήσετε την εξοικονόμηση για κλήσεις κινητής τηλεφωνίας , εγκαταστήστε το δωρεάν WEBTouch VOIP Client από το Google Play. Εάν δεν έχετε ακόμα λογαριασμό , κάντε εγγραφή στην ιστοσελίδα της Webtouch , πιστώστε στον λογαριασμό σας λίγα ευρώ και ξεκινήστε να μιλάτε με τις χαμηλότερες τιμές τις αγοράς Το μόνο που χρειάζεται είναι η εφαρμογή και μία 3G ή WiFi σύνδεση.";
     echo "</div>";
     
     echo "</div>";
